806a86e6a26b0e726529b0d45617f0063e32ad8e
nagxsan <sanchetng@gmail.com>
2025-03-14 01:01:39 +0530

    using iterators and closures

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ use std::process;
 use minigrep;
 
 fn main() {
-    // collect arguments from command line into a vector
-    let args: Vec<String> = env::args().collect();
-
     // store the args in descriptive variables
-    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
